c0cf138a011500f2d82074193b8d251d710db3cf
Ming <ming@alone.tw>
2018-07-13 18:19:00 +0800

    copycat_plugin.py Porting Python 2 Code to Python 3

--- copycat_plugin.py
+++ copycat_plugin.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# Copyright © 2013 george 
+# Copyright © 2013 george
 #
 # Distributed under terms of the MIT license.
 __all__ = ['copy', 'paste', 'list', 'delete']
@@ -22,24 +22,24 @@ def get_from_vim(name):
     return vim.eval(name)
 
 def set_to_vim(name, value):
-    if not isinstance(name, basestring):
-        return False
+    #if not isinstance(name, basestring):
+    #    return False
 
-    if isinstance(value, basestring):
-        value = re.sub(r'([\"\\])', r'\\\1', value)
-        vim.command('let {}=\"{}\"'.format(name, value))
-        return True
-    return False
-    
+    #if isinstance(value, basestring):
+    value = re.sub(r'([\"\\])', r'\\\1', value.decode('utf-8'))
+    vim.command('let {}=\"{}\"'.format(name, value))
+    return True
+    #return False
+
 def vim_command(func):
     def deco(result=None, *args, **kwargs):
         result_reg = result
         result = func(*args, **kwargs)
         if result_reg:
             set_to_vim(result_reg, result)
-        
+
     return deco
-    
+
 
 # ----------------------------------------------------
 # copycat vim command
